b9d5e0ba21ed1a0ece3c70307393854f3d0eaae9
kpardhasai2004 <94817466+kpardhasai2004@users.noreply.github.com>
2021-12-28 21:27:17 +0530

    Add files via upload

---
+++ Guess the number program.py
@@ -0,0 +1,17 @@
+print("choose a number between 1 to 10")
+
+print("Now add 330 to the choosen number ")
+
+print("Now subract 122 to the choosen number ")
+
+print("Now divide the choosen number by 3 ")
+
+print("Now multiply the choosn number by 2 ")
+
+x_1=input("Enter the number you got : ")
+
+result=((((float(x_1)/2)*3)+122)-330)
+
+result_1=round(result)
+
+print("The number you have choosen is :" + str(result_1))
