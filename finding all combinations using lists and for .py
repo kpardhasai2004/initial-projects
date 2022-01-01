49664bd10e5ec171ad9c9e38f94115a718725d17
kpardhasai2004 <94817466+kpardhasai2004@users.noreply.github.com>
2022-01-01 16:54:13 +0530

    Add file

---
+++ finding all combinations using lists and for .py
@@ -0,0 +1,8 @@
+meal=["biriyani","noodles","fried rice"]
+drink=["soda","milk","cola"]
+dessert=["cookies","donut","cup cake"]
+
+for M in meal:
+    for D in drink:
+        for DS in dessert:
+            print("I will have "+M+" as a meal "+"I also want "+D+" as a drink and "+DS+" as a dessert")
