1dba5509dcc02159232bd0fdc15581440418f3fb
kpardhasai2004 <94817466+kpardhasai2004@users.noreply.github.com>
2022-01-03 14:23:52 +0530

    Add files via upload

--- attendance eligibility checker.py
+++ attendance eligibility checker.py
@@ -1,16 +1,32 @@
 name=input("enter name of the student: ")
 reg=input("enter reg number: ")
-sub1_attendance=input("attendance of sub1: ")
-sub2_attendance=input("attendance of sub2: ")
-sub3_attendance=input("attendance of sub3: ")
-sub4_attendance=input("attendance of sub4: ")
-sub5_attendance=input("attendance of sub5: ")
+
+sub1_name=input("Name of sub1: ")
+
+sub2_name=input("Name of sub2: ")
+
+sub3_name=input("Name of sub3: ")
+
+sub4_name=input("Name of sub4: ")
+
+sub5_name=input("Name of sub5: ")
+
+sub1_attendance=input("attendance of " + sub1_name + " : ")
+
+sub2_attendance=input("attendance of " + sub2_name + " : ")
+
+sub3_attendance=input("attendance of " + sub3_name + " : ")
+
+sub4_attendance=input("attendance of " + sub4_name + " : ")
+
+sub5_attendance=input("attendance of " + sub5_name + " : ")
+
 minimum_attendance=60
 
 if float(sub1_attendance)>float(minimum_attendance) and float(sub2_attendance)>float(minimum_attendance) and float(sub3_attendance)>float(minimum_attendance) and float(sub4_attendance)>float(minimum_attendance) and float(sub5_attendance)>float(minimum_attendance):
     print("studet name : "+ name)
     print("student Reg_no : " + reg)
-    print("status: eligible to attend exam")  
+    print("status: eligible to attend exam ")  
 
 
 else :
