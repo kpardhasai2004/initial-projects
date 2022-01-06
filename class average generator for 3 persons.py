fc0c44824b61a395c58a507641337a17e8f3fe41
kpardhasai2004 <94817466+kpardhasai2004@users.noreply.github.com>
2022-01-06 14:45:25 +0530

    Add file

--- class average generator for 3 persons.py
+++ class average generator for 3 persons.py
@@ -35,9 +35,9 @@ stu3_sub_3=input("what are the marks of 3rd subject ? ")
 
 stu3_avgm= (float(stu3_sub_1)+float(stu3_sub_2)+float(stu3_sub_3)) / 3
 
-avgm=[ str(stu1_avgm)+ " is the average of " + stu1_name + "with reg no " + str(stu1_regno),
-str(stu2_avgm)+ " is the average of " + stu2_name + "with reg no " + str(stu2_regno),
-str(stu3_avgm)+ " is the average of " + stu3_name + "with reg no " + str(stu3_regno)]
+avgm=[ str(stu1_avgm)+ " is the average of " + stu1_name + " with reg no " + str(stu1_regno),
+str(stu2_avgm)+ " is the average of " + stu2_name + " with reg no " + str(stu2_regno),
+str(stu3_avgm)+ " is the average of " + stu3_name + " with reg no " + str(stu3_regno)]
         
 avgm.sort()
 print( 'list in assending order : ', avgm)
